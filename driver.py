81bfe194e852a78b2c4f7a2efde41d4dd6e7b61f
Jacob Johnson <jdjhc4@umsystem.edu>
2022-02-02 12:37:41 -0600

    pairCheck() finished up

--- driver.py
+++ driver.py
@@ -30,5 +30,5 @@ for x in range(0, 2):
     player5 = [deck[-16], deck[-17], deck[-18], deck[-19], deck[-20], 0]
     player6 = [deck[-21], deck[-22], deck[-23], deck[-24], deck[-25], 0]
 
-handChecker.highCard(myHand)
+handChecker.threeOfAKind(myHand)
 print(myHand)
--- handChecker.py
+++ handChecker.py
@@ -10,33 +10,38 @@ def checkHands(myHand, hand2, hand3, hand4, hand5, hand6):
     f.write(myHand)
     f.close
 
+def convertLists(listOfStrings):
+    listOfInts = []
+    convertibleStr = ''
+    for i in range(0, len(listOfStrings)-1):
+        convertibleStr += (listOfStrings[i] + " ")
+    listOfInts = (re.findall(r'\d+', convertibleStr))
+    listOfInts = [int(i) for i in listOfInts]
+    return listOfInts
+
 def highCard(handList):
-    numericalValues = []
-    handListStr = ''
-    for i in range(0, len(handList)-1):
-        handListStr += (handList[i] + " ")
-    numericalValues = (re.findall(r'\d+', handListStr))
-    numericalValues = [int(i) for i in numericalValues]
-    print(numericalValues)
-    highest = numericalValues[0]
-    print(highest)
-    for i in range(1, len(numericalValues)):
-        if numericalValues[i] > highest:
-            highest = numericalValues[i]
+    numericalVals = convertLists(handList)
+    highest = numericalVals[0]
+    for i in range(1, len(numericalVals)):
+        if numericalVals[i] > highest:
+            highest = numericalVals[i]
     handList[-1] += highest
 
-# def onePair(handList):
-#     pairBool = None
-#     for x in range(0, len(handList)):
-#       if there's a pair:
-#           handList[5] += (valueOfPair + 13) 
-#     return pairBool
+def pairCheck(handList):
+    numericalVals = convertLists(handList)
+    pairs = []
+    for i in numericalVals:
+        if numericalVals.count(i) == 2:
+            pairs.append(i)
+            numericalVals.remove(i)
+    if len(pairs) == 1:
+        handList[-1] += (pairs[0] + 13)
+    if len(pairs) == 2:
+        handList[-1] += (pairs[0] + pairs[1] + 23)
         
-# def twoPair(handList):
-#     for x in range(0, len(handList)):
-        
-# def threeOfAKind(handList):
-#     for x in range(0, len(handList)):
+
+def threeOfAKind(handList):
+    for x in range(0, len(handList)):
 
 
 # def straight(handList):
