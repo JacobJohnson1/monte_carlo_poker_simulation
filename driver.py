d3dbd639753112c2a3048bdd2177dcf9240d26fc
Jacob Johnson <jdjhc4@umsystem.edu>
2022-02-11 17:17:52 -0600

    calculates win percentage

--- driver.py
+++ driver.py
@@ -15,13 +15,8 @@ def dealMyHand(deck):
     myHand = [deck.pop(), deck.pop(), deck.pop(), deck.pop(), deck.pop(), 0]
     return myHand
 
-# # fakeHand = ['14H', '14S', '14D', '14C', '13H', 0]
-# fakeHand = ['12H', '12H', '11H', '9D', '9H', 0]
-# print(fakeHand)
-# handScorer.pairCheck(fakeHand)
-# print(fakeHand)
-
 def driverFunction():
+    numOfGames = 100
     deck = createDeck()
     random.shuffle(deck)
     myHand = dealMyHand(deck)
@@ -29,22 +24,14 @@ def driverFunction():
 
     myScore = handScorer.score(myHand)
     handScorer.labelHands(myHand)
-
-    for i in range(0, 100):
-        winCounter = 0
-
+    winCounter = 0
+    for i in range(0, numOfGames):
+        random.shuffle(deck)
         player2 = [deck[-1], deck[-2], deck[-3], deck[-4], deck[-5], 0]
         player3 = [deck[-6], deck[-7], deck[-8], deck[-9], deck[-10], 0]
         player4 = [deck[-11], deck[-12], deck[-13], deck[-14], deck[-15], 0]
         player5 = [deck[-16], deck[-17], deck[-18], deck[-19], deck[-20], 0]
         player6 = [deck[-21], deck[-22], deck[-23], deck[-24], deck[-25], 0]
-
-        # print(player2)
-        # print(player3)
-        # print(player4)
-        # print(player5)
-        # print(player6)
-
 
         Score2 = handScorer.score(player2)
         Score3 = handScorer.score(player3)
@@ -64,5 +51,7 @@ def driverFunction():
             winCounter += 0.5
 
         print(winCounter)
+    winPercentage = (winCounter/numOfGames * 100)
+    print(winPercentage)
 
 driverFunction()
